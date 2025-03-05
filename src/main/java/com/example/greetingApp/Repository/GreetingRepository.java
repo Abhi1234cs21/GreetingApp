4e03a5ab5aee564397c3eaca99b7d258f96517fe
Abhi1234cs21 <kamleshadajariya6252@gmail.com>
2025-03-05 11:11:07 +0530

    User Login and Registeration

--- src/main/java/com/example/greetingApp/Repository/GreetingRepository.java
+++ src/main/java/com/example/greetingApp/Repository/GreetingRepository.java
@@ -3,5 +3,5 @@ package com.example.greetingApp.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.greetingApp.Model.Greeting;
 
-public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+public interface  GreetingRepository extends JpaRepository<Greeting, Long> {
 }
